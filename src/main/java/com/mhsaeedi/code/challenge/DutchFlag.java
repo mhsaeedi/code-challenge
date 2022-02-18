f8d61bffdd408aab18a16acb2f3068e9800a3a7e
mhsaeedi <mh.saeedi@yahoo.com>
2022-02-18 01:46:46 +0100

    Update DutchFlag.java
    sth'

--- src/main/java/com/mhsaeedi/code/challenge/DutchFlag.java
+++ src/main/java/com/mhsaeedi/code/challenge/DutchFlag.java
@@ -1,10 +1,20 @@
 package com.mhsaeedi.code.challenge;
 
 public class DutchFlag{
-  public static solve(int[] arr){
+
+  public static void main(String[] args){
+    int[] arr = new int[]{0,2,1,1,2,0,
+    0,1,2,1,0,2};
+    solve(arr);
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]);
+    }
+  }
+
+  public static void solve(int[] arr){
     int i0=-1;
     int i1=-1;
-    int i2=-1.
+    int i2=-1;
     for(int i=0;i<arr.length;i++){
       if(arr[i]==0){
         if(i1!=-1 && i1<i) {
@@ -21,11 +31,15 @@ public class DutchFlag{
       }
       else if(arr[i]==1){
         if(i2!=-1 && i2<i){
+        
           swap(arr,i,i2);
           int temp = i2;
           i2 =i1;
-          i1 = temp
-        }
+          i1 = temp;
+        } else if(i1==-1) i1=i;
+      }
+      else if(arr[i]==2){
+        if(i2==-1) i2=i;
       }
       
     }
