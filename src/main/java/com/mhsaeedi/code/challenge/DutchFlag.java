2bacb7ea34201f40119d0dc6fd483137c9dac326
mhsaeedi <mh.saeedi@yahoo.com>
2022-02-20 16:28:05 +0100

    Update DutchFlag.java

--- src/main/java/com/mhsaeedi/code/challenge/DutchFlag.java
+++ src/main/java/com/mhsaeedi/code/challenge/DutchFlag.java
@@ -11,43 +11,17 @@ public class DutchFlag{
     }
   }
 
-  public static void solve(int[] arr){
-    int i0=-1;
-    int i1=-1;
-    int i2=-1;
-    for(int i=0;i<arr.length;i++){
-      if(arr[i]==0){
-        if(i1!=-1 && i1<i) {
-          swap(arr,i,i1);
-          int temp=i1;
-          i1 = i;
-          i0 = temp;
-        }else if(i2!=-1 && i2<i){
-          swap(arr,i,i2);
-          int temp=i2;
-          i2=i;
-          i0=temp;
-        }
-      }
-      else if(arr[i]==1){
-        if(i2!=-1 && i2<i){
-        
-          swap(arr,i,i2);
-          int temp = i2;
-          i2 =i1;
-          i1 = temp;
-        } else if(i1==-1) i1=i;
-      }
-      else if(arr[i]==2){
-        if(i2==-1) i2=i;
-      }
-      
-    }
+  public static void solve(int[] a){
+    int i=0,j=0,k=a.length-1;
+    while(j<=k)
+      if(a[j]==0)swap(a,i++,j++);
+      else if(a[j]==2)swap(a,j,k--);
+      elss j++;
   }
 
-  private static void swap(int[] arr, int i, int j){
-    int temp=arr[i];
-    arr[i]=arr[j];
-    arr[j]=temp;
+  private static void swap(int[] a, int i, int j){
+    int t=a[i];
+    a[i]=a[j];
+    a[j]=t;
   }
 }
